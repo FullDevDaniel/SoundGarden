d907a2969b36092ae4ffe24b397bd58ab92a8c14
Daniel Alejandro <danielzanlucas@gmail.com>
2023-01-11 23:20:25 -0300

    adicionando API para criação de eventos no servidor

--- js/cadastro.js
+++ js/cadastro.js
@@ -4,7 +4,9 @@ const atrações = document.querySelector("#atracoes");
 const descricao = document.querySelector("#descricao");
 const data = document.querySelector("#data");
 const lotacao = document.querySelector("#lotacao");
+const button = document.getElementsByClassName("btn btn-primary")[0];
 
+let btnSave = document.getElementById("btnSave");
 let fobs = document.getElementById('observação');
 let fatracao_msg = document.getElementById('msg-vld-fatracao');
 let fname_msg = document.getElementById('msg-vld-fname');
@@ -85,36 +87,100 @@ data.onblur = (event) => {
     }
 }
 
- lotacao.onblur = (event) => {
-     if (event.target.value.length < 1) {
-         fmsg_lotacao.textContent = "Insira um número.";
-         fmsg_lotacao.style.display = "block";
-         lotacao.classList.add("required"); //adicionar classe (com css é interessante)
-         event.preventDefault();
-     } else {
+lotacao.onblur = (event) => {
+    if (event.target.value.length < 1) {
+        fmsg_lotacao.textContent = "Insira um número.";
+        fmsg_lotacao.style.display = "block";
+        lotacao.classList.add("required"); //adicionar classe (com css é interessante)
+        event.preventDefault();
+    } else {
         fmsg_lotacao.style.display = "none"; //faz texto aparece!!!
     }
 }
 
-formCadastroCliente.addEventListener("submit", (form) => {
-    form.preventDefault(); //para não submeter
-    const { elements } = form.target;
-  
-    const result = [];
-  
-    for (const element of elements) {
-      if (element.name) {
-        result.push(`${element.name}: ${element.value}`);
-      }
+formCadastroCliente.onsubmit = (event) => {
+
+    if (nomeCompleto.value.length < 3 &&
+        atrações.value == '' &&
+        data.value == '' &&
+        descricao.value == '' &&
+        lotacao.value == '') {
+        alert("Por favor, preencha o formulário")
+        event.preventDefault(); //bloquear envio!!!
+    } else if (nomeCompleto.value.length < 3) {
+        nomeCompleto.focus()
+        alert("Por favor, informe o nome")
+        event.preventDefault(); //bloquear envio!!!
+    } else if (atrações.value == '') {
+        atrações.focus()
+        alert("Por favor, informe a atração")
+        event.preventDefault(); //bloquear envio!!!
+
+    } else if (descricao.value == '') {
+        descricao.focus()
+        alert("Por favor, escreva uma descrição")
+        event.preventDefault(); //bloquear envio!!!
     }
-  
-    alert("Mensagem enviada com sucesso! \n\n" + result.join("\n"));
-  
-    form.target.reset();
-  });
+    else if (data.value == '') {
+        data.focus()
+        alert("Por favor, informe a data")
+        event.preventDefault(); //bloquear envio!!!
+    } else if (lotacao.value == '') {
+        lotacao.focus()
+        alert("Por favor, informe a lotação")
+        event.preventDefault(); //bloquear envio!!!
+    } else {
+
+        event.preventDefault();
 
 
+        let name = formCadastroCliente.nome.value;
+        let atracoes = formCadastroCliente.atracoes.value;
+        let descricao = formCadastroCliente.descricao.value;
+        let data = formCadastroCliente.data.value;
+        let lotacao = formCadastroCliente.lotacao.value;
 
 
+        let attractions = atracoes.split(",");
+
+        let raw = {
+            "name": name,
+            "poster": "link da imagem",
+            "attractions": attractions,
+            "description": descricao,
+            "scheduled": data,
+            "number_tickets": lotacao,
+        };
 
 
+        function postEvent() {
+            let requestOptions = {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(raw),
+                redirect: "follow",
+            };
+
+            let endpoint = 'https://soundgarden-api.deta.dev/events';
+            fetch(endpoint, requestOptions)
+                .then(response => response.text())
+                .then(result => console.log(result))
+                .catch(error => console.log('error', error));
+
+            alert("Seu evento foi cadastrado com sucesso!");
+
+            document.querySelector("#nome").value = "";
+            document.querySelector("#atracoes").value = "";
+            document.querySelector("#descricao").value = "";
+            document.querySelector("#data").value = "";
+            document.querySelector("#lotacao").value = "";
+
+        }
+
+        postEvent();
+    };
+
+
+};
