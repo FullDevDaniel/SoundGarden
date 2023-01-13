9747d2f843e6a6043175ea4e2b6c7c7615cf414d
Daniel Alejandro <danielzanlucas@gmail.com>
2023-01-12 22:54:20 -0300

    incrementando codigo para puxar eventos direto da api

--- js/admin.js
+++ js/admin.js
@@ -1,40 +1,73 @@
-// Listar eventos
-const eventos = D.querySelector('#eventos')
+// // Listar eventos
+// const eventos = D.querySelector('#eventos')
 
-const options = {
-    method: 'GET',
-    redirect: 'follow'
-}
+// const options = {
+//     method: 'GET',
+//     redirect: 'follow'
+// }
 
-const selecionarEventos = async () => {
-    return await fetch(`${BASE_URL}/events`, options).then(response => {
-        return response.json(); 
-    }).then(result => result);
-}
+// const selecionarEventos = async () => {
+//     return await fetch(`https://soundgarden-api.deta.dev/events`, options).then(response => {
+//         return response.json(); 
+//     }).then(result => result);
+// }
 
-D.addEventListener('DOMContentLoaded', async () => {
+// D.addEventListener('DOMContentLoaded', async () => {
 
-    const resposta = await selecionarEventos();
+//     const resposta = await selecionarEventos();
 
-    const eventosApi = resposta.map( (item, id) => {
+//     const eventosApi = resposta.map( (item, id) => {
 
-        return `
-        <tr>
-        <th scope="row">${id+1}</th>
-        <td>${new Date(item.scheduled).toLocaleDateString(
-            "pt-BR"
-          )}</td>
-        <td>${item.name}</td>
-        <td>${item.attractions}</td>
-        <td>
-            <a href="#" class="btn btn-dark" data-toggle="modal" data-target="#modal"
-            data-whatever="'${item.name}'">ver reservas</a>
-            <a href="editar-evento.html?id=${item._id}" class="btn btn-secondary">editar</a>
-            <a href="excluir-evento.html?id=${item._id}" class="btn btn-danger">excluir</a>
-        </td>
-    </tr>
-        `
-    })
+//         return `
+//         <tr>
+//         <th scope="row">${id+1}</th>
+//         <td>${new Date(item.scheduled).toLocaleDateString(
+//             "pt-BR"
+//           )}</td>
+//         <td>${item.name}</td>
+//         <td>${item.attractions}</td>
+//         <td>
+//             <a href="#" class="btn btn-dark" data-toggle="modal" data-target="#modal"
+//             data-whatever="'${item.name}'">ver reservas</a>
+//             <a href="editar-evento.html?id=${item._id}" class="btn btn-secondary">editar</a>
+//             <a href="excluir-evento.html?id=${item._id}" class="btn btn-danger">excluir</a>
+//         </td>
+//     </tr>
+//         `
+//     })
     
-    eventos.innerHTML = eventosApi;
-})
+//     eventos.innerHTML = eventosApi;
+// })
+
+const tabela = document.querySelector("#tabelaEventos");
+const tabelaBody = document.querySelector("#tabela");
+const botaoEdit = document.querySelector("#editar-eventos");
+
+console.log(botaoEdit);
+
+async function getEventos(){
+    const response = await fetch('https://soundgarden-api.deta.dev/events')
+
+    const eventos = await response.json();
+
+   for(let i=0; eventos.length>i;i++){
+
+        const eventoTable = `
+        <tr>
+            <th scope="row">${[i]}</th>
+            <td>${new Date(eventos[i].scheduled).toLocaleDateString()} - ${new Date(eventos[i].scheduled).toLocaleTimeString()}</td>
+            <td>${eventos[i].name}</td>
+            <td>${eventos[i].attractions}</td>
+            <td>
+                <a href="reservas.html?id=${eventos[i]._id}" class="btn btn-dark">ver reservas</a>
+                <a href="editar-evento.html?id=${eventos[i]._id}" id= "editar-eventos" class="btn btn-secondary">editar</a>
+                <a href="excluir-evento.html?id=${eventos[i]._id}" class="btn btn-danger">excluir</a>
+            </td>
+        </tr>`
+
+        tabelaBody.innerHTML += eventoTable;
+   }     
+    
+}
+
+getEventos();
--- js/index.js
+++ js/index.js
@@ -38,3 +38,29 @@ const next = () => {
 document.querySelector('#previus').addEventListener('click', previus)
 document.querySelector('#next').addEventListener('click', next)
 itens = document.querySelectorAll('.item')
+
+//codigo para puxar eventos da API!!!
+
+const EventoCaixa = document.querySelector("#caixa_evento");
+
+async function getEventos() {
+    const response = await fetch('https://soundgarden-api.deta.dev/events')
+
+    const eventos = await response.json();
+
+    for (let i = 0; 3 > i; i++) {
+
+        const eventoTable = `
+        <article class="evento card p-5 m-3">
+            <h2>${eventos[i].name}</h2>
+            <h4>${eventos[i].attractions}</h4>
+            <p>${eventos[i].description}</p>
+            <a class="btn btn-primary" onclick="openModal('${eventos[i]._id}')">reservar ingresso</a>
+        </article>`
+
+        EventoCaixa.innerHTML += eventoTable;
+    }
+
+}
+
+getEventos();
